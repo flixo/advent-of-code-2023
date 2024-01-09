1cd1c86a6476ad06bb5b2870d513394fa19f9698
Jens <jens.nomtak@readly.com>
2024-01-09 12:55:22 +0100

    code

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,82 +1,176 @@
+use core::num;
+use std::collections::HashMap;
+
 advent_of_code::solution!(3);
+
+#[derive(Debug, Clone, Eq, Hash, PartialEq, Copy)]
+struct Point {
+    x: i32,
+    y: i32
+}
+
+#[derive(Debug)]
+struct Number {
+    value: u32,
+    occupation: Vec<Point>
+}
+
+impl Number {
+    fn neighbouring_coords(&self) -> Vec<Point> {
+        self.occupation.iter().flat_map(|point| {
+            vec![
+                Point { x: point.x + 1, y: point.y },
+                Point { x: point.x - 1, y: point.y },
+                Point { x: point.x, y: point.y + 1 },
+                Point { x: point.x, y: point.y - 1 },
+
+                Point { x: point.x + 1, y: point.y + 1 },
+                Point { x: point.x - 1, y: point.y - 1 },
+                Point { x: point.x + 1, y: point.y - 1 },
+                Point { x: point.x - 1, y: point.y + 1 },
+            ]
+        })
+        .filter(|coord| {
+            !self.occupation.contains(coord)
+        })
+        .unique()
+        .collect()
+    }
+}
+
+#[derive(Debug)]
+struct Symbol {
+    value: char,
+    coord: Point,
+    cog_val: Vec<u32>
+}
 
 #[derive(Debug)]
 struct Schematic {
-    size: (usize, usize),
-    data: Vec<char>,
-    to_inspect: Vec<usize>
+    width: i32,
+    height: i32,
+    numbers: Vec<Number>,
+    symbols: HashMap<Point, Symbol> ,
 }
 
 impl Schematic {
     fn from_input(input: &str) -> Schematic {
-        let height = input.lines().count();
-        let width = input.lines().next().unwrap().chars().count();
+        let height = input.lines().count() as i32;
+        let width = input.lines().next().unwrap().chars().count() as i32;
 
-        let mut to_inspect: Vec<usize> = vec![];
-        let mut data: Vec<char> = vec![];
+        let mut numbers: Vec<Number> = vec![];
+        let mut symbols: HashMap<Point, Symbol> = HashMap::new();
 
-        input.chars().filter(|c| !c.is_whitespace()).enumerate().for_each(|(i, c)| {
+        let mut x = 0;
+        let mut y = 0;
+
+        let mut number_sequence = String::new();
+        let mut occupation: Vec<Point> = vec![];
+
+        input.chars().for_each(|c| {
+            let point = Point { x, y };
+
             if c.is_numeric() {
-                to_inspect.push(i);
+                number_sequence.push(c);
+                occupation.push(point);
             }
-            if !c.is_whitespace() {
-                data.push(c);
+
+            if !c.is_numeric() && number_sequence.len() > 0 {
+                let number = number_sequence.parse::<u32>().unwrap();
+                
+                numbers.push(Number { 
+                    value: number, 
+                    occupation: occupation.clone()
+                });
+
+                occupation.clear();
+                number_sequence.clear();
+            }
+
+            if !c.is_numeric() && !c.is_ascii_control() && c != '.' {
+                symbols.insert(point, Symbol { value: c, coord: point, cog_val: vec![] });
+            }
+
+            if c == '\n' {
+                x = 0;
+                y += 1;
+            } else {
+                x += 1;
             }
         });
 
         Schematic { 
-            size: (width, height),
-            data: data,
-            to_inspect: to_inspect
+            width,
+            height,
+            numbers,
+            symbols
         }
     }
-
-    fn idx_to_coord(&self, idx: usize) -> (usize, usize) {
-        let (width, _) = self.size;
-        let x = idx % width;
-        let y = idx / width;
-
-        (x, y)
-    }
-
-    fn coord_to_idx(&self, coord: (usize, usize)) -> usize {
-        let (width, _) = self.size;
-        let (x, y) = coord;
-
-        y * width + x
-    }
-    
-    fn data_at(&self, coord: (usize, usize)) -> char {
-        let idx = self.coord_to_idx(coord);
-        self.data[idx]
-    }
-
-    fn data_at_idx(&self, idx: usize) -> char {
-        self.data[idx]
-    }
 }
-
-
 
 pub fn part_one(input: &str) -> Option<u32> {
     let schematic = Schematic::from_input(input);
 
-    schematic.to_inspect.iter().for_each(|idx| {
-        let coord = schematic.idx_to_coord(*idx);
+    let ans: u32 = schematic.numbers.iter().filter(|number| {
+        number.neighbouring_coords().iter().any(|coord| {
+            schematic.symbols.contains_key(coord)
+        })
+    }).map(|number| number.value).sum();
 
-        //vec![(1,0),()]
-
-        println!("{:?}", coord);
-    });
-
-    //println!("{:?}", schematic.data_at((2, 2)));
-
-
-    None
+    Some(ans)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
+    let mut schematic = Schematic::from_input(input);
+
+    schematic.numbers.iter().for_each(|number| {
+        number.neighbouring_coords().iter().for_each(|coord| {
+            if let Some(symbol) = schematic.symbols.get_mut(coord) {
+                if symbol.value == '*' {
+                    symbol.cog_val.push(number.value);
+                }
+            }
+        });
+    });
+
+    let ans: u32 = schematic.symbols.values().filter(|symbol| {
+        println!("{:?}", symbol.cog_val);
+        symbol.cog_val.len() == 2
+    }).map(|symbol| {
+        symbol.cog_val[0] * symbol.cog_val[1]
+    }).sum();
+
+    println!("{:?}", ans);
+
+    // schematic.numbers.iter().map(|number| {
+    //     let cogs = number.neighbouring_coords().iter().filter(|coord| {
+    //         if let Some(symbol) = schematic.symbols.get(coord) {
+    //             if symbol.value == '*' {
+    //                 true
+    //             } else {
+    //                 false
+    //             }
+    //         } else {
+    //             false
+    //         }
+    //     }).collect();
+
+    //     if cogs.count() == 2 {
+    //         let c1 = cogs.next().unwrap();
+    //         let c2 = cogs.next().unwrap();
+    //         0
+    //     } else {
+    //         0
+    //     }
+    // });
+
+
+
     None
+    //number.neighbouring_coords().iter().any(|coord| {
+    //
+    //    let sym = schematic.symbols.get(coord);
+    //})
 }
 
 #[cfg(test)]
@@ -86,7 +180,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4361));
     }
 
     #[test]
